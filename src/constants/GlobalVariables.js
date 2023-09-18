46a9bd00d31c248fff72523e1e22b5f6130990df
naman <Nmndwdi1001@gmail.com>
2023-09-18 18:21:52 +0530

    backend url

--- src/constants/GlobalVariables.js
+++ src/constants/GlobalVariables.js
@@ -1,7 +1,8 @@
 class GlobalVariables
 {
     // static uri='http://localhost:4000';
-    static uri = 'http://192.168.0.104:4000';
+    // static uri = 'http://192.168.0.104:4000';
+    static uri = 'https://qrit-backend.vercel.app';
 }
 
 export default GlobalVariables;
